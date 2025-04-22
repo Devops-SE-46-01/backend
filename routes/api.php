64c10ad5696ba0ae28c13526a9fc6b2f14528f7c
Yazid Al Ghozali <yazid.al2418@gmail.com>
2025-04-22 20:22:05 +0700

    add create asistant laboratory

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AslabController;
 use App\Http\Controllers\Api\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,6 +25,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/recruitation/check/{nim}', [RecruitationController::class, 'check']);
 Route::post('/recruitation', [RecruitationController::class, 'store']);
 Route::get('/yazidal/blogs', [BlogController::class, 'index'])->name('blogs.index');
+Route::delete('/yazidal/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
+Route::post('/yazidal/aslab', [AslabController::class, 'store'])->name('blogs.store');
+Route::get('/yazidal/aslab', [AslabController::class, 'index'])->name('aslab.index');
 // Route::prefix('api')->group(function () {
 //     Route::get('/blogs', [BlogController::class, 'index']);
 // });
