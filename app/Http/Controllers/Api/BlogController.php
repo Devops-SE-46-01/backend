7a7ab850e1aff743be83cd890530c898280ae0f1
Fehehehe <febryt22@gmail.com>
2025-04-22 19:58:31 +0700

    feat: adding BlogController template

---
+++ app/Http/Controllers/Api/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+
+    public function edit() {}
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {}
+}
